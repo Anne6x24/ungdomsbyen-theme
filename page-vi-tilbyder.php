0902e3536e0075399713f26dc5b4fdb2197dcb65
“Anne6x24” <Anne6x24@stud.kea.dk>
2021-11-07 15:04:10 +0100

    hi

--- page-vi-tilbyder.php
+++ page-vi-tilbyder.php
@@ -14,30 +14,7 @@ get_headers();
     <article class="kasse_1">
         <p class="beskrivelse_1"></p>
     </article>
-    <article class="kasse_2">
-        <p class="klassetrin"></p>
-        <p class="varighed"></p>
-        <p class="deltagere"></p>
-        <p class="indhold"></p>
-        <p class="indhold_b"></p>
-    </article>
-    <article class="kasse_3">
-        <h3 class="overskrift_2"></h3>
-        <p class="tekst_1"></p>
-    </article>
-    <article class="kasse_4">
-        <h3 class="overskrift_3"></h3>
-        <p class="tekst_2"></p>
-    </article>
-    <article class="kasse_5">
-        <h3 class="overskrift_4"></h3>
-        <p class="tekst_3"></p>
-    </article>
-    <article class="kasse_6">
-        <h3 class="overskrift_5"></h3>
-        <p class="tekst_4"></p>
-        
-    </article>
+
 </template> 
 
 <section id="primary" class="content-area">
