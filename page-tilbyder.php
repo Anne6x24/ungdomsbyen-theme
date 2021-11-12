9aa44b2c9dff3516e6273aefc5e6fd7743fce515
“Anne6x24” <Anne6x24@stud.kea.dk>
2021-11-12 09:20:22 +0100

    hi

--- page-tilbyder.php
+++ page-tilbyder.php
@@ -3,28 +3,139 @@
 get_header();
 ?>
 
+<style>
+
+body:not(.home) .site-content {
+    background-color:#C4D2C7;
+}
+
+nav {
+    text-align: center;
+}
+
+button {
+    background-image: none;
+    /* background-color: #019C51; */
+    background-color: #FFFFFF;
+    border-style: solid;
+    border-width: 2px 2px 2px 2px;
+    border-color: #000000;
+    color: black;
+    margin: 5px;
+    padding: 5px 10px;
+}
+
+button:hover {
+    background-image: none;
+    background-color: #FAD8E8;
+    
+}
+
+.brobygning {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+}
+
+
+
+#kursus_oversigt {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+    margin-top: 4rem;
+    margin-left: 5rem;
+    margin-right: 5rem;
+}
+
+#kursus_oversigt{
+    grid-column: 1/2;
+    grid-row: 1;
+    background-color: #F6F2E4;
+    padding: 34px;
+    margin: 130px;
+    margin-top: 20px;
+}
+
+#toppen {
+    text-align: center;
+    
+}
+
+
+
+#toppen p {
+    font-size: 20px;
+    margin-left: 15rem;
+    margin-right: 15rem;
+}
+
+
+
+p {
+    font-family: Galvji;
+    font-size: 10px; 
+}
+
+h2, h4 {
+    color: black;
+}
+
+h3 {
+    font-size: 22px;
+}
+
+h4 {
+    font-size: 15px;
+}
+</style>
+
+
 <section id="primary" class="content-area">
     <main id="main" class="site-main">
-    
-    <nav id="filtrering"><button class="filter_valgt" data_cont="alle">Alle</button></nav>
-    <nav id="indhold_filtrering"><button class="filter_valgt" data_cont="alle">Alle</button></nav>
+    <section id="toppen">
+    <h1>VI TILBYDER</h1>
+    <p>Vi tilbyder kurser, som henvender sig til elever såvel som
+undervisere, skoler og kommuner. Vores Kurser omhandler
+alt fra konflikhåndtering, FN’s verdensmål og økonomi.
+Nedenfor ser du en oversigt over de kurser vi tilbyder.</p>
+    <div class="brobygning">
+    <h3>BROBYGNING OG ERHVERVSUDDANNELSER</h3>
+    <p>Udover diverse kurser, tilbyder vi også brobygning i forskellige former. Læs 
+        mere om dette via nedenstående links.
+    </p>
+    <a href="https://ungdomsbyen.dk/wp-content/uploads/2020/03/ungby-erhvervsfolder-20-21.pdf">Katalog vedrørende brobygningsforløb</a>
+    </div>
+    </section> 
+    <nav id="filtrering"><button class="filter_valgt" data-kursus="alle">Alle</button></nav>
+  
+   
+    <!-- <nav id="indhold_filtrering"><button class="filter_valgt" data-cont="alle">Alle</button></nav> -->
     
     <section id="kursus_oversigt"></section>
     </main>
     </section>
 
+
+
+
+
+
 <template>
     <article class="kasse_1">
-    <h2 class="kategori"></h2>
-        <h3 class="navn"></h3>
-        <h4 class="kategori_2"></h4>
-        <h5 class="kursus"></h5>
+        
+    <h3 class="navn"></h3>
+    <h4 class="kategori"></h2>
         <p class="beskrivelse"></p>
         <p class="pris"></p>
         <p class="henvendelse"></p>
+        <button class="her1">LÆS MERE</button>
+        <button clas="her2">BOOK HER</button>
+        
     </article>
 
 </template> 
+
 
 
 <script>
@@ -63,21 +174,22 @@ get_header();
             visKurser();
             opretKnapper();
         }
+
             function opretKnapper(){
             
             categories.forEach(cat=>{
                //console.log(cat.id);
                 if(cat.name != "Uncategorized"){
-                document.querySelector("#filtrering").innerHTML += `<button class="filter_valgt" data-kursus="${cat.id}">${cat.name}</button>`
+                document.querySelector("#filtrering").innerHTML += `<button class="filter" data-kursus="${cat.id}">${cat.name}</button>`
                 
                 }
             })
 
-            indhold.forEach(cont=>{
-               console.log(cont.id);
-                document.querySelector("#indhold_filtrering").innerHTML += `<button class="filter_valgt" data-cont="${cont.id}">${cont.name}</button>`
+            // indhold.forEach(cont=>{
+            //    console.log(cont.id);
+            //     document.querySelector("#indhold_filtrering").innerHTML += `<button class="filter" data-cont="${cont.id}">${cont.name}</button>`
             
-            })
+            // })
 
             addEventListenersToButtons();
         }
@@ -89,15 +201,16 @@ get_header();
             console.log({filterIndhold});
             kurser.forEach(kursus => {
                 //tjek filterRet og filterIndhold til filtrering
-                if ((filterKursus == "alle"  || kursus.categories.includes(parseInt(filterKursus))) && (filterIndhold == "alle"  || kursus.indhold.includes(parseInt(filterIndhold)))) {
+                if ((filterKursus == "alle"  || kursus.categories.includes(parseInt(filterKursus))) && (filterIndhold == "alle"  || kursus.hovedkategori.includes(parseInt(filterIndhold)))) {
                     const klon = skabelon.cloneNode(true).content;
+                    klon.querySelector(".navn").innerHTML = kursus.navn;
                     klon.querySelector(".kategori").innerHTML = kursus.kategori;
-            klon.querySelector(".navn").innerHTML = kursus.navn;
+            
             klon.querySelector(".beskrivelse").innerHTML = kursus.beskrivelse;
             klon.querySelector(".pris").innerHTML = kursus.pris;
             klon.querySelector(".henvendelse").innerHTML = kursus.henvendelse;
-                    klon.querySelector("article").addEventListener("click", () => {
-                        location.href = kursus.link;
+            klon.querySelector(".her1").addEventListener("click", () => {
+                location.href = kursus.link;
                     })
                     liste.appendChild(klon);
                 }
@@ -108,14 +221,14 @@ get_header();
             document.querySelectorAll("#filtrering button").forEach(elm => {
                 elm.addEventListener("click", filtrering);
             })
-             document.querySelectorAll("#indhold_filtrering button").forEach(elm => {
-                elm.addEventListener("click", filtreringIndhold);
-            })
+            //  document.querySelectorAll("#indhold_filtrering button").forEach(elm => {
+            //     elm.addEventListener("click", filtreringIndhold);
+            // })
         }
         
         function filtrering() {
             filterKursus = this.dataset.kursus;
-            document.querySelector("h2").textContent = this.textContent;
+            
             //fjern .valgt fra alle
             document.querySelectorAll("#filtrering .filter").forEach(elm => {
                 elm.classList.remove("valgt");
@@ -125,23 +238,24 @@ get_header();
             this.classList.add("valgt");
             visKurser();
         }
-         function filtreringIndhold() {
-            filterIndhold = this.dataset.kursus;
-            document.querySelector("h2").textContent = this.textContent;
-             //fjern .valgt fra alle
-            document.querySelectorAll("#indhold_filtrering .filter").forEach(elm => {
-                elm.classList.remove("valgt");
-            });
-            //tilføj .valgt til den valgte
-            this.classList.add("valgt");
-            visKurser();
-        }
+        //  function filtreringIndhold() {
+        //     filterIndhold = this.dataset.cont;
+           
+        //      //fjern .valgt fra alle
+        //     document.querySelectorAll("#indhold_filtrering .filter").forEach(elm => {
+        //         elm.classList.remove("valgt");
+        //     });
+        //     //tilføj .valgt til den valgte
+        //     this.classList.add("valgt");
+        //     visKurser();
+        // }
     </script>
 
 	</section><!-- #primary -->
 
 <?php
-get_footer();
+get_footer(); ?>
+<?php get_sidebar();?>
 
 
 
--- single-kursus.php
+++ single-kursus.php
@@ -1,82 +1,67 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'enqueue_important_files' );
-function enqueue_important_files() {
-/*hent parent stylesheet i parenttemaets mappe*/
-wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
-?>
+
 
 <?php
-
-get_headers();
+get_header();
 ?>
-<template>
-    <article class="kasse_1">
-        <p class="beskrivelse_1"></p>
-    </article>
-    <article class="kasse_2">
-        <p class="klassetrin"></p>
-        <p class="varighed"></p>
-        <p class="deltagere"></p>
-        <p class="indhold"></p>
-        <p class="indhold_b"></p>
-    </article>
-    <article class="kasse_3">
-        <h3 class="overskrift_2"></h3>
-        <p class="tekst_1"></p>
-    </article>
-    <article class="kasse_4">
-        <h3 class="overskrift_3"></h3>
-        <p class="tekst_2"></p>
-    </article>
-    <article class="kasse_5">
-        <h3 class="overskrift_4"></h3>
-        <p class="tekst_3"></p>
-    </article>
-    <article class="kasse_6">
-        <h3 class="overskrift_5"></h3>
-        <p class="tekst_4"></p>
-        
-    </article>
-</template> 
 
-<section id="primary" class="content-area">
-    <main id="main" class="site-main">
-    
-    <section id="kursus_indhold"></section>
+    <section id="primary" class="content-area">
+        <main id="single" class="site-main">
+        <button class="luk">Tilbage</button>
+        <section class="indhold">
+            <article class="enkeltKursus">
+                <p class="kortbeskrivelse"></p>
+                <h4 class="klassetrin">Klassetrin:</h4>
+                <h4 class="varighed">Varighed:</h4>
+                <h4 class="fag">Fag:</h4>
+                <h4 class="deltagere">Antal deltagere:</h4>
+                <h4 class="indhold">Indhold:</h4>
+                <p class="pris"></p>
+                <button class="book"></button>
+                
+                <p class="navn"></p>
+            </article>
+        </section>
     </main>
 
-<script>
-        let kurser = [];
-        const side = document.querySelector("#kursus_indhold");
-        const indhold = document.querySelector("template");
-        document.addEventListener("DOMContentLoaded", start);
+    <script>
+        //find parametre (variabler) i url'en
+       // let urlParams = new URLSearchParams(window.location.search);
+        //returner værdien for variablen "id" 
+       // let id = urlParams.get("id");
+        let kursus;
+        document.addEventListener("DOMContentLoaded", getJson);
 
-        function start() {
-            getJson();
+        async function getJson() {
+            console.log("id er", <?php echo get_the_ID() ?>);
+            //hent en enkelt ret udfra id'et
+            let jsonData = await fetch("http://annemunksgaard.dk/kea/02sem/tema09/ungdomsbyen_wp/wp-json/wp/v2/kursus/" + <?php echo get_the_ID() ?>);
+            kursus = await jsonData.json();
+            visKursus();
+            console.log(kursus);
+        }
+        //vis data om retten
+        function visKursus() {
+            
+            document.querySelector(".kortbeskrivelse").innerHTML = kursus.beskrivelse;
+            document.querySelector(".klassetrin").innerHTML = kursus.antal_deltagere;
+            document.querySelector(".varighed").innerHTML = kursus.varighed;
+            document.querySelector(".indhold").innerHTML = kursus.indhold;
+            document.querySelector(".pris").innerHTML = kursus.pris;
+            
         }
 
-
-    const url = "http://annemunksgaard.dk/kea/02sem/tema09/ungdomsbyen_wp/wp-json/wp/v2/vi-tilbyder?per_page=100";
-    async function getJson () {
-        let response = await fetch(url);
-        kurser = await response.json();
-        visKurser();
-        console.log(Kurser);
-    }
-
-    function visKurser () {
-        console.log(kurser);
-        kurser.forEach(kursus => {
-            const klon = indhold.cloneNode(true).content;
-            klon.querySelector(".beskrivelse_1").textContent = kursus.beskrivelse;
-
-            side.appendChild(klon);
-
+        document.querySelector(".luk").addEventListener("click", () => {
+            //link tilbage til den foregående side på "luk" knappen
+            history.back();
         })
-    }
-
-
     </script>
+
+        
+    </section><!-- #primary -->
+
+<?php
+
+get_footer(); ?>
+<?php get_sidebar();?>
 
 
